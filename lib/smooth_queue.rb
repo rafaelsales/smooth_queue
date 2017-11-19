9ae38beeb2d564c57fd7c53a8b4b3f85d20cdd2b
Rafael Sales <rafaelcds@gmail.com>
2017-11-18 21:17:10 -0300

    Create redis and namespaced helpers. Refactor RedisHelpers

--- lib/smooth_queue.rb
+++ lib/smooth_queue.rb
@@ -3,9 +3,11 @@ require 'redis-namespace'
 require 'connection_pool'
 require_relative 'smooth_queue/config'
 require_relative 'smooth_queue/util'
+require_relative 'smooth_queue/redis_helpers'
 
 module SmoothQueue
   PRIORITIES = %i[head tail].freeze
+  REDIS_NS = 'squeue'.freeze
 
   def self.configure(&_block)
     @config ||= Config.new
@@ -17,10 +19,11 @@ module SmoothQueue
   end
 
   def self.wait_for_work
-    with_redis do |redis|
+    with_nredis do |redis|
       redis.subscribe('queue_changed') do |on|
         on.message do |_channel, queue|
-          queue_changed(queue)
+          puts redis.llen(processing_queue)
+          queue_changed(queue, redis)
         end
       end
     end
@@ -30,7 +33,7 @@ module SmoothQueue
     max_concurrency = config.queue_max_concurrency(queue)
     processing_queue = Util.processing_queue(queue)
 
-    with_redis do |redis|
+    with_nredis do |redis|
       binding.pry
       # TODO: Use lua to make next two lines atomic
       if redis.llen(processing_queue) < max_concurrency
@@ -53,7 +56,7 @@ module SmoothQueue
 
     payload = Util.build_message_payload(queue, message)
     id = Util.generate_id
-    with_redis do |redis|
+    with_nredis do |redis|
       redis.sadd('queues', queue)
     end
     add_payload_to_queue(id, payload, queue)
@@ -61,7 +64,7 @@ module SmoothQueue
 
   # Removes the message from processing queue as it was successfully processed
   def self.done(id)
-    with_redis do |redis|
+    with_nredis do |redis|
       payload = Util.from_json(redis.hget('messages', id))
       raise ArgumentError, "`id` doesn't match an existing message" unless payload
 
@@ -78,7 +81,7 @@ module SmoothQueue
   # Moves the message back to the waiting queue
   # NOTE: Redesign to support retry delay
   def self.retry(id)
-    with_redis do |redis|
+    with_nredis do |redis|
       payload = Util.from_json(redis.hget('messages', id))
     end
     payload['retry_count'] = payload.fetch('retry_count', 0) + 1
@@ -90,15 +93,20 @@ module SmoothQueue
     end
   end
 
+  def self.redis_connection_pool
+    @redis_connection_pool ||= ConnectionPool::Wrapper.new(size: 10) { Redis.new }
+  end
+
   def self.with_redis(&block)
-    @redis_pool ||= ConnectionPool::Wrapper.new(size: 10) do
-      Redis::Namespace.new('smooth_queue', redis: Redis.new)
-    end
-    @redis_pool.with(&block)
+    redis_connection_pool.with(&block)
+  end
+
+  def self.with_nredis(&_block)
+    with_redis { |redis| yield(Redis::Namespace.new(REDIS_NS, redis: redis)) }
   end
 
   def self.add_payload_to_queue(id, payload, queue, &_block)
-    with_redis do |redis|
+    with_nredis do |redis|
       redis.multi do
         redis.hset('messages', id, payload)
         redis.lpush(queue, id)
--- lib/smooth_queue/redis_helpers.rb
+++ lib/smooth_queue/redis_helpers.rb
@@ -1,46 +1,52 @@
 module SmoothQueue
   class RedisHelpers
-    SCRIPTS = {
-      pop_message_to_process: {
-        script: <<-LUA.freeze,
-          local queue = KEYS[1]
-          local processing_queue = KEYS[2]
-          local max_concurrency = KEYS[3]
+    class Script < Struct.new(:code)
+      def call(keys: [], args: [])
+        SmoothQueue.with_nredis do |redis|
+          redis.evalsha(sha, keys, args)
+        end
+      rescue Redis::CommandError => e
+        if e.message =~ /NOSCRIPT/
+          load_script
+          retry
+        else
+          raise
+        end
+      end
 
-          if redis.call('LLEN', processing_queue) < tonumber(max_concurrency) then
-            local id = redis.call('RPOPLPUSH', queue, processing_queue)
-            return id
-          else
-            return nil
-          end
-        LUA
-      }
-    }
+      def load_script
+        SmoothQueue.with_redis do |redis|
+          @sha = redis.script(:load, code).freeze
+        end
+      end
+
+      def sha
+        load_script unless @sha
+        @sha
+      end
+    end
+
+    SCRIPTS = {
+      pop_message_to_process: Script.new(<<-LUA.freeze),
+        local queue = KEYS[1]
+        local processing_queue = KEYS[2]
+        local max_concurrency = ARGV[1]
+
+        if redis.call('LLEN', processing_queue) < tonumber(max_concurrency) then
+          local id = redis.call('RPOPLPUSH', queue, processing_queue)
+          return id
+        else
+          return nil
+        end
+      LUA
+    }.freeze
 
     def self.pop_message_to_process(queue, processing_queue, max_concurrency)
-      call(:pop_message_to_process, keys: [queue, processing_queue, max_concurrency])
+      call(:pop_message_to_process, keys: [queue, processing_queue], args: [max_concurrency])
     end
 
-    def self.call(name, keys: [], args: [])
-      SmoothQueue.with_redis do |redis|
-        redis.evalsha(script_sha(name), keys, args)
-      end
-    rescue Redis::CommandError => e
-      if e.message =~ /NOSCRIPT/
-        script_sha(name, reload: true)
-        retry
-      else
-        raise
-      end
-    end
-
-    def self.script_sha(name, reload: false)
-      SmoothQueue.with_redis do |redis|
-        if hash.key?(:sha) || reload
-          hash[:sha] = redis.script(:load, hash.fetch(:script))
-        end
-        hash.fetch(:sha)
-      end
+    def self.call(name, **arguments)
+      SCRIPTS.fetch(name).call(**arguments)
     end
   end
 end
